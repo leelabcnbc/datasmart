f4819df17405aa3065c33e83f46e62e211a0842d
Yimeng Zhang <zym1010@gmail.com>
2016-10-29 13:09:32 -0400

    remove versions in env script

--- install_python_env.sh
+++ install_python_env.sh
@@ -8,4 +8,4 @@ conda create -y --name datasmart python=3
 pip install -r requirements.txt
 
 # then install those visualization related stuff. not needed for travis, but good for everyday use.
-conda install -y -c conda-forge pandas=0.18.1 notebook=4.2.2 numpy=1.11.1
+conda install -y -c conda-forge pandas notebook numpy
